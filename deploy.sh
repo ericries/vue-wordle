0d805b45aac63e0928b5f0eb4e39988c0cbc2691
eries2 <EricRies2@MacBook-Air-13-2020.local>
2022-03-05 12:43:31 -0800

    deploy script that works

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,16 @@ set -e
 # build
 npm run build
 
+# if you are deploying to a custom domain
+#echo 'encandle.org' > CNAME
+
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+git add --all
+git commit -m "Deploy to gh-pages"
+git push origin gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ericries/vue-wordle-encantle.git main:gh-pages
 
 cd -
